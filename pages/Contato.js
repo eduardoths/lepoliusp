5310658108bc019905c86d58329889bc6d97a979
Eduardo <santos.eduardothomaz@gmail.com>
2021-02-16 19:50:23 -0300

    Adicionado formulario de contato (nao funciona, apenas html e css)

--- pages/Contato.js
+++ pages/Contato.js
@@ -1,5 +1,77 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faMailBulk, faMapMarkerAlt } from  '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faInstagram, faInstagramSquare, faLinkedinIn, faMailchimp } from '@fortawesome/free-brands-svg-icons';
+
 function Contato() {
-  return <div>Contato</div>
+  return (
+  <div className="contato">
+    <div className="container">
+        <h3>Tem alguma dúvida?</h3>
+        <h1>Fale com a gente</h1>
+        <div className="row">
+          <div className="col-3">
+            <span className="contato-texto contato-destaque">
+              <FontAwesomeIcon icon={faMapMarkerAlt} width="10"/> Escola Politécnica da USP
+            </span>
+            <span className="contato-texto">&nbsp;</span>
+            <span className="contato-texto">
+              Av. Prof Luciano Gualberto, 380
+            </span>
+            <span className="contato-texto">
+              Butantã, São Paulo - SP
+            </span>
+            <span className="contato-texto">&nbsp;</span>
+            <span className="contato-texto contato-destaque">
+              Redes sociais e email:
+            </span>
+            <span className="contato-texto contato-texto-2">
+              <FontAwesomeIcon icon={ faFacebookF } height="1.2rem"/> <a target="_blank" href="https://facebook.com/lepoliusp">facebook.com/lepoliusp</a>
+            </span>
+            <span className="contato-texto contato-texto-2">
+              <FontAwesomeIcon icon={ faInstagram } height="1.2rem"/> <a target="_blank" href="https://instagram.com/lepoliusp">@lepoliusp</a>
+            </span>
+            <span className="contato-texto contato-texto-2">
+              <FontAwesomeIcon icon={ faLinkedinIn } height="1.2rem"/> <a href="https://www.linkedin.com/company/lepoliusp/">linkedin.com/company/lepoliusp</a>
+            </span>
+            <span className="contato-texto contato-texto-2">
+              <FontAwesomeIcon icon={ faEnvelope } height="1.2rem"/> <a href="mailto:lepoliusp@gmail.com">lepoliusp@gmail.com</a>
+            </span>
+          </div>
+          <div className="col-9">
+            <form method="POST">
+            <div className="row">
+              <div className="col-6 contato-input">
+                <span>Nome</span>
+                <input type="text"/>
+              </div>
+              <div className="col-6 contato-input">
+                <span>Sobrenome</span>
+                <input type="text"/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 contato-input">
+                <span>Email</span>
+                <input type="email"/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 contato-input">
+                <span>Mensagem</span>
+                <textarea type="text" id="mensagem"></textarea>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 contato-input">
+                <button type="submit">Enviar</button>
+              </div>
+            </div>
+            </form>
+          </div>
+        </div>
+    </div>
+  </div>
+  )
 }
 
 export default Contato;
