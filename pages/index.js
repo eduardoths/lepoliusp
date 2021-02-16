389082c170513d18df56c2c64809013f7df044b0
Eduardo <santos.eduardothomaz@gmail.com>
2021-02-15 23:20:28 -0300

    Tela inicial sem imagens adicionada

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 function Home() {
   return (
   <>
-  <div className="inicio">
-    <div>
-      Home
+    <div className="inicio">
+      <div className="inicio-card">
+        <div className="inicio-card-titulo">
+          Somos a Liga de Empreendedorismo da Poli    
+        </div>
+        <div className="inicio-card-texto">
+          Temos como missão <strong>inspirar</strong>, <strong>conectar</strong> e <strong>capacitar </strong> 
+          futuros empreendedores e empreendedoras politécnicos.<br/>
+          Venha fazer parte!
+        </div>
+      </div>
     </div>
-  </div>
+    <div className="linhas-divisoria">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
   </>
   );
 }
